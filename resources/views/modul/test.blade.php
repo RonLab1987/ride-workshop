3a5b59778fe6f1b791a021104348f56a89423a50
RonLab1987 <ronlab@yandex.ru>
2015-12-20 16:46:57 +0300

    #1

--- resources/views/modul/test.blade.php
+++ resources/views/modul/test.blade.php
@@ -2,6 +2,7 @@
     <div class="container text-center">
         <h2>Web-приложение "КАТАЙ"</h2>
         <p>Framework's: Laravel, Bootstrap, jQuery(or AngularJS)</p>
+        <p>Branch: RomanLaptev</p>
         <p>старт: 20.12.2015</p>
     </div>
 </div>
