93625638ac8b199419365210ef39755f897e67ba
RonLab1987 <ronlab@yandex.ru>
2016-01-06 20:12:15 +0300

    #допилил страницу принятой заявки

--- resources/views/public/module/thanks-header.blade.php
+++ resources/views/public/module/thanks-header.blade.php
@@ -4,18 +4,20 @@
                 <div class="col-lg-6 col-md-6 col-sm-9 col-xs-12">
                     <div class="thanks-message">
                     <h1 class="text-white">заявка принята</h1>
-                    
+                    <?php
+                       // $input = array('name'=>'василий'); $phone='+7 (912) 337-01-02'
+                    ?>
                     <blockquote>
                         <p class="lead text-white">
                             <span class="text-uppercase">{{ $input['name'] }}, в ближайшее время мы перезвоним вам по телефону <span class="phone">{{ $phone }}</span>.</span>
-                            <br><em>А сейчас предлагаю сделать перерыв и посмотреть несколько интересных и мотивирующих видео о мире велоспорта. </em>
+                            <br><em>А сейчас предлагаю сделать перерыв и посмотреть несколько интересных и мотивирующих видео о веломире. </em>
                         </p>
-                        <footer class="text-white">Роман, веломастерская "Катай!"</footer>
+                        <!--footer class="text-white">Роман, веломастерская "Катай!"</footer-->
                     </blockquote>
                     </div>
                     
                     
-                    <a href="#thanks-video" class="page-scroll btn btn-xl btn-uci-green ">да, у меня есть 5 минут <span class="glyphicon glyphicon-chevron-right "></span></a>
+                    <a href="#thanks-video" class="page-scroll btn btn-xl btn-uci-green "><span class="hidden-xs">у меня</span> есть 5 минут <span class="glyphicon glyphicon-chevron-right "></span></a>
                 </div>
             
             
--- resources/views/public/module/thanks-video.blade.php
+++ resources/views/public/module/thanks-video.blade.php
@@ -1,36 +1,35 @@
 <section class="thanks-video" id="thanks-video">
     <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <label class=" text-muted text-center">кросс кантри</label>
-                <iframe width="100%" height="300px" src="https://www.youtube.com/embed/DwTy3N5T9uw?list=PL99O-99OJ-Ny-3poeLKE-4ktacHASXioA" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-               <label class=" text-muted">шоссейные гонки</label> 
-               <iframe width="100%" height="300px" src="https://www.youtube.com/embed/rWO8D-2TrJw" frameborder="0" allowfullscreen></iframe>
-            </div> 
-            
+        <div class="row text-center">
+            <h2 class="section-heading">5 Минут</h2>
+            <h3 class="section-subheading">паралельных миров</h3>
         </div>
+        
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <label class=" text-muted">велопутешествия</label>
-                <iframe width="100%" height="300px" src="https://www.youtube.com/embed/XoCIrMv5fEs?list=PLMad0UYgJgxcAjBvyzZEyqsjMzj8G9YoQ" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-               <label class=" text-muted">велокросс</label> 
-               <iframe width="100%" height="300px" src="https://www.youtube.com/embed/gCgxz3WgsF0" frameborder="0" allowfullscreen></iframe>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <!--p class="lead text-center">ПУТЕШЕСТВУЙ</p-->
+                <p class="lead text-center text-uci-blue">#ПУТЕШЕСТВИЯ</p>
+                <iframe width="100%" height="550" src="https://www.youtube.com/embed/XoCIrMv5fEs" frameborder="0" showinfo=0  allowfullscreen></iframe>
+                <p class="text-center text-uci-blue"><span class="glyphicon glyphicon-chevron-down"></span></p>
             </div> 
         </div>
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <label class=" text-muted">велопутешествия</label>
-                <iframe src="//vk.com/video_ext.php?oid=-86529149&id=172030316&hash=00f2874f0a8f2474&hd=2" width="100%" height="300"  frameborder="0"></iframe>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-               <label class=" text-muted">велокросс</label> 
-               <iframe width="100%" height="300px" src="https://www.youtube.com/embed/gCgxz3WgsF0" frameborder="0" allowfullscreen></iframe>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                
+                <p class="lead text-center text-uci-yellow">#БОРЬБА</p>
+                <iframe width="100%" height="550" src="https://www.youtube.com/embed/_AD2Q88ADys" frameborder="0" showinfo=0  allowfullscreen></iframe>
+                <p class="text-center text-uci-yellow"><span class="glyphicon glyphicon-chevron-down"></span></p>
             </div> 
         </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <p class="lead text-center text-uci-green   ">#РАЗВЛЕЧЕНИЯ</p>
+                <iframe width="100%" height="550" src="https://www.youtube.com/embed/DwTy3N5T9uw" frameborder="0" showinfo=0  allowfullscreen></iframe>
+                <p class="text-center text-uci-green"><span class="glyphicon glyphicon-chevron-down"></span></p>
+            </div> 
+        </div>
+        
+       
     </div>
 </section>
 
