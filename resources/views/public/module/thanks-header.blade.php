f153f698920546280b60c28981a9b9f9bfed2b94
RonLab1987 <ronlab@yandex.ru>
2016-01-10 13:28:59 +0300

    CE8

--- resources/views/public/module/thanks-header.blade.php
+++ resources/views/public/module/thanks-header.blade.php
@@ -2,22 +2,35 @@
     <div class="row">
         <div class="container">
                 <div class="col-lg-6 col-md-6 col-sm-9 col-xs-12">
-                    <div class="thanks-message">
+                    <!--div class="thanks-message">
                     <h1 class="text-white">заявка принята</h1>
                     <?php
                         $input = array('name'=>'василий'); $phone='+7 (912) 337-01-02'
                     ?>
-                    <blockquote>
+                    <-blockquote>
                         <p class="lead text-white">
                             <span class="text-uppercase">{{ $input['name'] }}, в ближайшее время мы перезвоним вам по телефону <span class="phone">{{ $phone }}</span>.</span>
-                            <br><em>А сейчас предлагаю сделать перерыв и посмотреть несколько интересных и мотивирующих видео о веломире. </em>
+                            <br>
+                            А сейчас предлагаю сделать перерыв и посмотреть несколько интересных и мотивирующих видео о веломире. 
                         </p>
-                        <!--footer class="text-white">Роман, веломастерская "Катай!"</footer-->
+                       
                     </blockquote>
+                    
+                    
+                    </div-->
+                    <div class="intro-text">
+                        <div class="intro-lead-in">заявка принята</div>
+                        <blockquote>
+                            <p class="lead text-uppercase">
+                                А сейчас предлагаю сделать перерыв и посмотреть несколько интересных и мотивирующих видео о веломире. 
+                            </p>
+                        </blockquote>
+                        <a href="#thanks-video" class="page-scroll  ">Посмотреть <span class="glyphicon glyphicon-chevron-right "></span></a>
+                        
                     </div>
                     
                     
-                    <a href="#thanks-video" class="page-scroll btn btn-xl btn-uci-green "><span class="hidden-xs">у меня</span> есть 5 минут <span class="glyphicon glyphicon-chevron-right "></span></a>
+                    
                 </div>
              
             
