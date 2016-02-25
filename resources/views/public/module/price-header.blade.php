fb40086e048a868a849be2138992502bd86f53f6
RonLab1987 <ronlab@yandex.ru>
2016-02-26 01:55:29 +0300

    #front

--- resources/views/public/module/price-header.blade.php
+++ resources/views/public/module/price-header.blade.php
@@ -2,9 +2,9 @@
         <div class="container">
             <div class="intro-text">
                 
-                <div class="intro-heading">доставка</div>
-                <div class="intro-lead-in text-uppercase"> от 0 до 100 рублей.<!--span class="glyphicon glyphicon-rub "></span--></div>
-                <!--div class="intro-lead-in">мы осблуживаем велосипед </div-->
+                <h1>доставка</h1>
+                <h2> от 0 до 100 рублей.</h2>
+                
                 
                 <a href="#price-list" class="page-scroll btn btn-xl btn-uci-blue">ПОДРОБНЕЕ</a>
                 <a href="{!! route('homepage') !!}#contact-form" class=" btn btn-xl btn-uci-green ">оформить заявку <span class="glyphicon glyphicon-chevron-right "></span></a>
