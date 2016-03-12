00826c2ca26e2d498cad532a468c789ac1031f6d
RonLab1987 <ronlab@yandex.ru>
2016-03-12 11:57:56 +0300

    change header section

--- resources/views/public/module/header.blade.php
+++ resources/views/public/module/header.blade.php
@@ -3,12 +3,19 @@
     <div class="container">
             <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 intro-text">
                 <div class="intro-area" >
-                    <img class="" src="/image/logo.svg" style=" width: 100%; padding: 25px 0; " >
+                <span class="contact">
+                    <span class="glyphicon glyphicon-phone-alt"></span>43-45-95<br>
+                    <a class="page-scroll" href="#contact-data">все контакты</a>
+                </span>
+                <h1>Вело&shy;мастерская </h1>
                 
-                    <div class="h-container" style="">
-                    <h1>Вело&shy;мастерская </h1>
-                    <h2>РЕМОНТ&ensp;И&ensp;ОБСЛУЖИВАНИЕ ВЕЛОСИПЕДОВ В г.&nbsp;КИРОВ </h2>
+                <div class="h-container">
+                    <img class="" src="/image/logo.svg" title="Веломастерская «КАТАЙ!»" >
+                    <p>
+                        Катайтесь, а обслуживание велосипеда доверьте нам
+                    </p>
                 </div>
+                 <h2>РЕМОНТ&ensp;И&ensp;ОБСЛУЖИВАНИЕ ВЕЛОСИПЕДОВ В г.&nbsp;КИРОВ С ВЫЕЗДОМ К ВАШЕМУ ДОМУ </h2>    
                 <div class="a-container">
                     <a href="#services" class="page-scroll btn btn-xl btn-uci-blue">ПОДРОБНЕЕ</a>
                     <a href="#contact-form" class="page-scroll btn btn-xl btn-uci-green ">оформить заявку <span class="glyphicon glyphicon-chevron-right hidden-xs"></span></a>
