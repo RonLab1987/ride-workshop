8b3cfb817a78b08e4f55f5f3619f22004945db8d
RonLab1987 <ronlab@yandex.ru>
2016-04-20 21:54:24 +0300

    add yandex metrica

--- resources/views/public/module/js-include.blade.php
+++ resources/views/public/module/js-include.blade.php
@@ -31,3 +31,5 @@
        yepnope('/agency/js/agency.js');
        yepnope('/agency/js/contact-form-controll.js');
     </script-->
+    
+    <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter36915905 = new Ya.Metrika({ id:36915905, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/36915905" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
